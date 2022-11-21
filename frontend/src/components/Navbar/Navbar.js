558fba2172ab3062039255be8424179df0626273
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2022-11-21 15:12:51 -0600

    Update Navbar redirect links

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,16 +2,20 @@ import './Navbar.css';
 import React from 'react';
 import logo from '../../images/logo.png';
 
-function Navbar() {
+const Navbar = () => {
+  const redirectHandler = (ref) => {
+    window.location.href=`${ref}`;
+  }
+
   return (
     <div className='Navbar'>
-      <div className='Logo'>
+      <div className='Logo' onClick={() => redirectHandler('/')}>
         <img src={logo}/>
         <h1>Seat Yourself!</h1>
       </div>
       <div className='Login'>
-        <p style={{textAlign: 'center'}}>Already have an account?<br/><a href="https://github.com/PaulKuruvila" rel='noopener noreferrer' target='_blank'>Click here.</a></p>
-        <button/>
+        <p>Already have an account?<br/><a href="/login" rel='noopener noreferrer'>Click here.</a></p>
+        <button onClick={() => redirectHandler('/signup')}/>
       </div>
     </div>
   );
