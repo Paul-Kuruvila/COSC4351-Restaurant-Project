03a0cce92fc78f95a93efad88aa02e49bf326f58
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2022-11-20 23:21:58 -0600

    Update sign up button and add link for login page

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,8 +1,17 @@
 import './Navbar.css';
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../images/logo.png';
 
 function Navbar() {
+  /*const [buttonText, setButtonText] = useState('Login');  This code is just sample code that was used for testing purposes
+
+  const changeButtonText = () => {
+    if(buttonText === 'Login')
+      setButtonText('Create an account');
+    else
+      setButtonText('Login');
+  };*/
+
   return (
     <div className='Navbar'>
       <div style={{display: 'flex', alignItems: 'center', cursor: 'help'}}>
@@ -10,8 +19,8 @@ function Navbar() {
         <h1 className='Title'>Seat Yourself!</h1>
       </div>
       <div className='Login'>
-        <p>Already have an account?</p>
-        <button />
+        <p style={{textAlign: 'center'}}>Already have an account?<br/><a href="https://github.com/PaulKuruvila" rel='noopener noreferrer' target='_blank'>Click here.</a></p>
+        <button/>
       </div>
     </div>
   );
