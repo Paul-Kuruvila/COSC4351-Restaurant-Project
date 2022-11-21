749f0c61b6faac3fef2e779cf4d36bce2718a8cb
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2022-11-21 15:15:25 -0600

    Install react-router-dom dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 const App = () => {
     return (
-        <div className="App">
-            <Navbar />
-            <Home />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className='content'>
+                    <Routes>
+                        <Route exact path = "/" element={<Home />} />
+                        <Route path = "/login" element={<Login />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
     );
 }
 
