d6df8db2032fac1e8246e1193ce71769ac3c85ff
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2022-11-25 02:08:50 -0600

    Clean up CSS for components in general

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -9,61 +9,60 @@ const Login = () => {
   const [validCred, setValidity] = useState(true);
 
   const handleSubmit = async (e) => { //sending data
-      e.preventDefault();
-      const loginData = {username, password};
-      const options = {
-          method: 'POST',
-          headers: {"Content-Type": "application/json"},
-          //credentials: "include",
-          body: JSON.stringify(loginData)
-      };
+    e.preventDefault();
+    const loginData = {username, password};
+    const options = {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        //credentials: "include",
+        body: JSON.stringify(loginData)
+    };
 
-      const response = await fetch('/auth', options)
-      const jsonData = await response.json();
+    const response = await fetch('/auth', options)
+    const jsonData = await response.json();
 
-      setValidity(jsonData.login);
+    setValidity(jsonData.login);
       
-      if (jsonData.login) { //if login status is true/successful
-          console.log(jsonData);
-          navigate.push('/') //redirect to home page
-          document.location.reload('true');
-      }
-      else {
-          console.log(jsonData);
-      }
+    if (jsonData.login) { //if login status is true/successful
+        console.log(jsonData);
+        navigate.push('/') //redirect to home page
+        document.location.reload('true');
+    } else {
+        console.log(jsonData);
+    }
   }
   return (
-      <div className="Login">
-          <form onSubmit = {handleSubmit}>
-              <ul className="signup-boxes">
-                  <li className='usernameArea'>
-                      <label className="userLabel">Username:</label>
-                      <input id="username" className="inputbox" type="text" title="Please enter your username." required placeholder="Enter your username."
-                      onChange = {(e) => setUsername(e.target.value)}
-                      onSelect = {() => setValidity(true)}
-                      />
-                  </li>
-                  <li className='passwordArea'>
-                      <label className="passLabel">Password:</label>
-                      <input id="password" className="inputbox" type="password" title="Please enter your password." required placeholder="Enter your password."
-                      onChange = {(e) => setPassword(e.target.value)}
-                      onSelect = {() => setValidity(true)}
-                      />
-                  </li>
-                  <li>
-                      <label className={validCred ? "hideInvalid" : "showInvalid"}>Invalid username/password. Please try again.</label>
-                  </li>
-                  <li>
-                      <button className="Submit" type="submit">Login</button>
-                  </li>
-                  <div className="signup-text">
-                      <p>Don't have an account?
-                          <br/><a href="/register">Sign up here</a>.
-                      </p>
-                  </div>
-              </ul>
-          </form>
-      </div>
+    <div className="Login">
+        <form onSubmit = {handleSubmit}>
+            <ul className="information-boxes">
+                <li className='usernameArea'>
+                    <label className="Label">Username:</label>
+                    <input id="username" className="inputbox" type="text" title="Please enter your username." required placeholder="Enter your username."
+                    onChange = {(e) => setUsername(e.target.value)}
+                    onSelect = {() => setValidity(true)}
+                    />
+                </li>
+                <li className='passwordArea'>
+                    <label className="Label">Password:</label>
+                    <input id="password" className="inputbox" type="password" title="Please enter your password." required placeholder="Enter your password."
+                    onChange = {(e) => setPassword(e.target.value)}
+                    onSelect = {() => setValidity(true)}
+                    />
+                </li>
+                <li>
+                    <label className={validCred ? "hideInvalid" : "showInvalid"}>Invalid username/password. Please try again.</label>
+                </li>
+                <li>
+                    <button className="Submit" type="submit">Login</button>
+                </li>
+                <div className="Login-Options">
+                    <p>Don't have an account?
+                        <br/><a href="/register">Sign up here</a>.
+                    </p>
+                </div>
+            </ul>
+        </form>
+    </div>
   );
 }
 
--- frontend/src/components/Reserve/Reserve.js
+++ frontend/src/components/Reserve/Reserve.js
@@ -1,71 +1,68 @@
-import React, {useState} from 'react'
-import './Reserve.css'
+import React, {useState} from 'react';
+import './Reserve.css';
 
-function Reserve() {
-    const redirectHandler = (ref) => {
-        window.location.href=`${ref}`;
-      }
-      const [name, setName] = useState();
-      const [phonenum, setPhonenum] = useState();
-      const [email, setEmail] = useState();
-      const [guests, setGuests] = useState();
+const Reserve = () => {
+	//number of guests and date n time 
+	const [name, setName] = useState();
+	const [phonenum, setPhonenum] = useState();
+	const [email, setEmail] = useState();
+	const [guests, setGuests] = useState();
 
-      //number of guests and date n time 
+	const redirectHandler = (ref) => {
+		window.location.href=`${ref}`;
+	}
 
-      const handleSubmit = async (e) => { //sending data
-        e.preventDefault();
-        const options = {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            //credentials: "include",
-            //body: JSON.stringify(loginData)
-        };
-  
-        const response = await fetch('/auth', options)
-        const jsonData = await response.json();
+	const handleSubmit = async (e) => { //sending data
+		e.preventDefault();
+		const options = {
+			method: 'POST',
+			headers: {"Content-Type": "application/json"},
+			//credentials: "include",
+			//body: JSON.stringify(loginData)
+		};
 
-    }
-    
-      return (
-        <div className='Reserve'>
-            <form onSubmit = {handleSubmit}>
-                  <ul className="information-boxes">
-                    <li className='fullname'>
-                          <label className="name">Name:</label>
-                          <input id="names" className="inputbox" type="text" title="Please enter your name." required placeholder="Enter your name."
-                          onChange = {(e) => setName(e.target.value)}
-                          />
-                    </li>
-                    <li className='phone'>
-                          <label className="phonenum">Phone Number:</label>
-                          <input id="phonenumber" className="inputbox" type="text" title="Please enter your phone number." required placeholder="Enter your phone number."
-                          onChange = {(e) => setPhonenum(e.target.value)}
-                          />
-                    </li>
-                    <li className='e-mail'>
-                          <label className="emailinfo">Email:</label>
-                          <input id="email" className="inputbox" type="text" title="Please enter your email." required placeholder="Enter your email."
-                          onChange = {(e) => setEmail(e.target.value)}
-                          />
-                    </li>
-                    <li className='numguests'>
-                          <label className="number of guests">Number of Guests:</label>
-                          <input id="guests" className="inputbox" type="text" title="Please enter the number of guests." required placeholder="Enter the number of guests."
-                          onChange = {(e) => setGuests(e.target.value)}
-                          />
-                    </li>
-                    <form>
-                        <label className = "labelofDT">Date and Time:</label>
-                        <input type = "datetime-local" id = "DT" name = ""></input>
-                    </form>
-                    <li>
-                           <button className="Submit" type="submit">Submit</button> 
-                    </li>
-                  </ul>
-            </form>
-        </div>
-      )
+		const response = await fetch('/auth', options);
+	}
+
+	return (
+		<div className='Reserve'>
+			<form onSubmit = {handleSubmit}>
+				<ul className='information-boxes'>
+					<li className='guest-info'>
+						<label className='Label'>Name:</label>
+						<input className='inputbox' type='text' title='Please enter your name.' required placeholder='Enter your name.'
+						onChange = {(e) => setName(e.target.value)}
+						/>
+					</li>
+					<li className='guest-info'>
+						<label className='Label'>Phone Number:</label>
+						<input className='inputbox' type='text' title='Please enter your phone number.' required placeholder='Enter your phone number.'
+						onChange = {(e) => setPhonenum(e.target.value)}
+						/>
+					</li>
+					<li className='guest-info'>
+						<label className='Label'>Email:</label>
+						<input className='inputbox' type='text' title='Please enter your email.' required placeholder='Enter your email.'
+						onChange = {(e) => setEmail(e.target.value)}
+						/>
+					</li>
+					<li className='guest-info'>
+						<label className='Label'>Number of Guests:</label>
+						<input className='inputbox' type='text' title='Please enter the number of guests.' required placeholder='Enter the number of guests.'
+						onChange = {(e) => setGuests(e.target.value)}
+					/>
+					</li>
+						<form>
+							<label className='Label'>Date and Time:</label>
+							<input type='datetime-local' id='DT' name=''></input>
+						</form>
+					<li>
+						<button className='Submit' type='submit'>Submit</button> 
+					</li>
+				</ul>
+			</form>
+		</div>
+	)
 }
 
-export default Reserve
-
+export default Reserve;
