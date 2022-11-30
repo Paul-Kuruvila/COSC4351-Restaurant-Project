21dee6fff1e6fd85f9b75623b94a81e1c2b50189
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2022-11-30 14:37:41 -0600

    Add slideshow to home page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,46 @@
 import './Home.css';
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+const backgroundColors = ['#FFB399', '#FFFF99','#E6B333', '#999966', '#99FF99',
+'#E6B3B3', '#B366CC', '#80B300', '#1AB399', '#809900', '#66664D', '#CC80CC']; 
 
 const Home = () => {
   const navigate = useNavigate();
 
+  const [index, setIndex] = useState(0);
+  const timeOut = useRef(null);
+  
+  function resetTimeout() {
+    if(timeOut.current) {
+      clearTimeout(timeOut);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeOut.current = setTimeout(
+      () => setIndex((prevIndex) =>
+        prevIndex === backgroundColors.length - 1 ? 0 : prevIndex + 1
+      ), 2500
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
   return (
     <div className='Home'>
+      <div className='slideshow-container'>
+        <div className='slider'
+          style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+        >
+          {backgroundColors.map((backgroundColor, index) => (
+            <div className='slide' key={index} style={{ backgroundColor: backgroundColor }}/>
+          ))}
+        </div>
+      </div>
       <button className='ReserveButton' onClick ={() => navigate('/reserve')}>RESERVE SEATING HERE!</button>
     </div>
   );
